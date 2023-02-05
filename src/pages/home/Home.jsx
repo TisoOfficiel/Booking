ea26938a728c0c5171fb18ff04630003725ccd15
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-02-05 14:43:02 +0100

    feat : add homepagelist

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,24 @@
-import { Header } from "../../components/header/Header"
-import { Navbar } from "../../components/navbar/Navbar"
+import Featured from "../../components/featured/Featured"
+import Header from "../../components/header/Header"
+import Navbar from "../../components/navbar/Navbar"
+import PropertyList from "../../components/propertyLIst/PropertyList"
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import "./home.css"
 
-export const Home = () => {
+const Home = () => {
   return (
     <div>
-      <Navbar />
-      <Header />
+    <Navbar/>
+    <Header />
+    <div className="homeContainer">
+      <Featured />
+      <h1 className="homeTitle">Browse by property type</h1>
+      <PropertyList />
+      <h1 className="homeTitle">Homes guest love</h1>
+      <FeaturedProperties />
     </div>
+  </div>
   )
 }
+
+export default Home
